3e7323bd53d99a8321ed83eae937009975729ddb
Lars Kemper <lk297162@fh-muenster.de>
2025-04-15 22:32:49 +0200

    build: move experimental gui code

--- scripts/format.sh
+++ scripts/format.sh
@@ -17,6 +17,6 @@ for path in "${IGNORED_PATHS[@]}"; do
     EXCLUDE_FIND+=" -path \"$path\" -prune -o"
 done
 
-CLANG_FORMAT_STYLE="{BasedOnStyle: LLVM, IndentWidth: 4, ColumnLimit: 120}"
+CLANG_FORMAT_STYLE="{BasedOnStyle: LLVM, IndentWidth: 2, ColumnLimit: 120}"
 
 eval "find . $EXCLUDE_FIND -type f \( -name '*.c' -o -name '*.h' -o -name '*.java' \) -print" | xargs clang-format --verbose --style="$CLANG_FORMAT_STYLE" --fallback-style=none -i
---
+++ src/main/RobotoApplication.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+
+public class Main {
+  public static void main(String[] args) {
+    LCD.clear();
+    LCD.drawString("Waiting for BT", 0, 0);
+    NXTConnection conn = Bluetooth.waitForConnection();
+
+    LCD.clear();
+    LCD.drawString("Connected", 0, 0);
+
+    DataOutputStream dos = conn.openDataOutputStream();
+    DataInputStream dis = conn.openDataInputStream();
+
+    try {
+      while (true) {
+        if (Button.LEFT.isDown()) {
+          LCD.clear();
+          LCD.drawString("Stopped by user", 0, 0);
+          break;
+        }
+
+        if (dis.available() > 0) {
+          String receivedData = dis.readLine();
+
+          LCD.clear();
+          LCD.drawString("Received:", 0, 0);
+          LCD.drawString(receivedData, 0, 1);
+
+          dos.writeBytes(receivedData);
+          dos.flush();
+
+          LCD.drawString("Sent back:", 0, 3);
+          LCD.drawString(receivedData, 0, 4);
+        } else {
+          LCD.drawString("No data", 0, 1);
+        }
+
+        Thread.sleep(50);
+      }
+    } catch (IOException ioe) {
+      LCD.clear();
+      LCD.drawString("IO Exception", 0, 0);
+      LCD.drawString(ioe.getMessage(), 0, 1);
+    } catch (InterruptedException ie) {
+    } finally {
+      try {
+        dos.close();
+        dis.close();
+        conn.close();
+      } catch (IOException ioe) {
+        LCD.clear();
+        LCD.drawString("Close Exception", 0, 0);
+        LCD.drawString(ioe.getMessage(), 0, 1);
+      }
+    }
+  }
+}
