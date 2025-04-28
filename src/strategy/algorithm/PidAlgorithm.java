f715320986dad8ac858788620c8b58b8c507d9a9
Tom Steinbach <tostein04@gmail.com>
2025-04-28 19:12:42 +0200

    feat: add PidAlgorithm

--- src/strategy/algorithm/PidAlgorithm.java
+++ src/strategy/algorithm/PidAlgorithm.java
@@ -1,28 +1,80 @@
 package strategy.algorithm;
 
 import util.Log;
+import lejos.util.PIDController;
+import core.RoboController;
+import io.actuator.IMotorController;
+import io.sensor.DefaultSensorValues;
 
 public class PidAlgorithm implements IFollowingAlgorithm {
-  public PidAlgorithm() {
-    // TODO: implement
+
+  /**
+   * The speed at which the robot moves forward.
+   */
+  private static final int TARGET_SPEED = DefaultSensorValues.MOTOR_MEDIUM_SPEED.getValue();
+
+  /**
+   * The speed factor used for turning the robot.
+   * This factor is used to adjust the speed of the outer wheel during turns.
+   */
+  private static final int LINE_EDGE = DefaultSensorValues.LIGHT_TRANSITION_MID.getValue();
+
+  /**
+   * The RoboController instance used to control the robot.
+   */
+  private final RoboController controller;
+
+  /**
+   * The motor controller used to control the motors of the robot.
+   */
+  private final IMotorController motorController;
+
+  /**
+   * The PID controller used to control the robot's speed and direction.
+   */
+  private PIDController pid;
+
+  public PidAlgorithm(RoboController controller) {
+    if (controller == null || controller.getContext().getMotorController() == null) {
+      throw new NullPointerException();
+    }
+
+    this.controller = controller;
+    this.motorController = controller.getContext().getMotorController();
+
+    this.pid = new PIDController(LINE_EDGE, 0);
+    this.pid.setPIDParam(PIDController.PID_KP, 10f);
+    this.pid.setPIDParam(PIDController.PID_KI, 0f);
+    this.pid.setPIDParam(PIDController.PID_KD, 20f);
   }
 
   @Override
   public void initialize() {
     Log.info("PidAlgorithm initialized");
 
-    // TODO: implement
+    this.motorController.stopMotors(true);
   }
 
   @Override
   public void deinitialize() {
     Log.info("PidAlgorithm deinitialize");
 
-    // TODO: implement
+    this.motorController.stopMotors(true);
   }
 
   @Override
   public void run() {
-    // TODO: implement
+    int currentLightValue = this.controller.getContext().getLastLightSensorValue();
+
+    if (currentLightValue == -1) {
+      return;
+    }
+
+    int turn = this.pid.doPID(currentLightValue);
+
+    int leftSpeed = TARGET_SPEED + turn;
+    int rightSpeed = TARGET_SPEED - turn;
+
+    this.motorController.forward(leftSpeed, rightSpeed);
   }
 }
